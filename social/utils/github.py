08c137dc7e16e9e86f92099fa41bf35f06b51498
Andrey Marakulin <annndruha.github@gmail.com>
2023-07-19 19:54:17 +0300

    Move to separate folder and add GH schema

--- social/utils/github.py
+++ social/utils/github.py
@@ -21,11 +21,5 @@ class Github:
             self.q_add_to_scrum = gql(f.read())
 
     def add_to_scrum(self, issue_id):
-        try:
-            params = {'projectId': self.settings.GH_SCRUM_ID,
-                      'contentId': issue_id}
-            r = self.client.execute(self.q_add_to_scrum, operation_name='AddToScrum', variable_values=params)
-            item_id = r['addProjectV2ItemById']['item']['id']
-            logging.info(f'Node {issue_id} successfully added to scrum with contentId= {issue_id}')
-        except Exception as err:
-            logging.error(f'Scrum adding FAILED: {err.args}')
+        params = {'projectId': self.settings.GH_SCRUM_ID, 'contentId': issue_id}
+        return self.client.execute(self.q_add_to_scrum, operation_name='AddToScrum', variable_values=params)
