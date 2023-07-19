649463ab8e8d068f56ca7585313320b639749e3c
Roman Dyakov <ROMAN@DYAKOV.SPACE>
2023-07-19 20:47:42 +0300

    Github API

--- social/github/profcomff_issues.py
+++ social/github/profcomff_issues.py
@@ -1,13 +1,25 @@
 import logging
+from datetime import datetime, timedelta
+
 from .base import event
+from social.utils.github_api import github
 
 
 logger = logging.getLogger(__name__)
 
 
-@event(issue=..., action="assigned")
+PROJECT_NODE_ID = "PVT_kwDOBaPiZM4AFiz-"
+DEADLINE_FIELD_NODE_ID = "PVTF_lADOBaPiZM4AFiz-zgHTmbk"
+TAKEN_FIELD_NODE_ID = "PVTF_lADOBaPiZM4AFiz-zgHTme8"
+
+
+@event(issue=..., action="opened")
 def issue_assigned(event):
-    """Если у задачи появился исполнитель – переведи задачу в статус "В работе" и установи время
-    начала работы (если не указано), срок выполнения через неделю (если меньше текущего)
-    """
-    logger.debug("Issue %s assigned", event["issue"].get("url"))
+    logger.debug("Issue %s created (node_id=%s)", event["issue"].get("url"), event["issue"].get("node_id"))
+    r = github.request_gql(
+        'social/github/profcomff_issues.gql',
+        'AddToScrum',
+        projectId=PROJECT_NODE_ID,
+        contentId=event["issue"].get("node_id")
+    )
+    logging.debug("Response %s", r)
