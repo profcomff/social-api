011fcfcc05ceba9dcef6a003c64b27de45dca7b8
Roman Dyakov <ROMAN@DYAKOV.SPACE>
2023-12-29 15:15:08 +0300

    Ping response to allow webhooks

--- social/routes/discord.py
+++ social/routes/discord.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi import APIRouter, BackgroundTasks, Request
+from fastapi.responses import JSONResponse
 from fastapi_sqlalchemy import db
 
 from social.handlers_discord.base import process_event
@@ -26,6 +27,9 @@ async def discord_webhook(request: Request, background_tasks: BackgroundTasks):
         )
     )
     db.session.commit()
+
+    if request_data.get("type") == 1:
+        return JSONResponse({"type": 1})
 
     background_tasks.add_task(process_event, request_data)
 
